4de31d9b057bb20416e8130fdd469bca4ec50488
GLOBAL\I337732 <irene.xu01@sap.com>
2017-03-17 16:41:31 +0800

    test flask flat

---
+++ freeze.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.insert(0, 'project')
+
+from project import main
+
+if __name__ == '__main__':
+    main.freezer.freeze()
---
+++ run.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.insert(0, 'project')
+
+import os
+from project import main
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    main.app.run(host='0.0.0.0', port=port)
